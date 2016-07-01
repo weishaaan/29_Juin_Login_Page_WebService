5f0a11f07df995359affc5381af12c702fda5f35
weishan <weishaaan@gmail.com>
2016-07-01 22:44:52 +0200

    finish jndi+login page

--- src/main/java/com/mora/login_page_29juin/MySQLConnect.java
+++ src/main/java/com/mora/login_page_29juin/MySQLConnect.java
@@ -1,17 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mora.login_page_29juin;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
-/**
- * Servlet implementation class MySQLConnect
- */
 
 public class MySQLConnect extends HttpServlet {
 
@@ -19,29 +18,70 @@ public class MySQLConnect extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String role;
+        Context initCtx = null;
+        Context envCtx = null;
+        DataSource ds = null;
+        Connection connection = null;
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String user = request.getParameter("user");
-        String pass = request.getParameter("pass");
+        String user_input = request.getParameter("user");
+        String pass_input = request.getParameter("pass");
+        
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/loginDB?useUnicode=true&"
-                            + "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
-                            + "serverTimezone=UTC&autoReconnect=true&useSSL=false", "root", "root");
-            PreparedStatement pst = conn.prepareStatement("Select user,pass from login where user=? and pass=?");
-            pst.setString(1, user);
-            pst.setString(2, pass);
+            initCtx = new InitialContext();
+            envCtx = (Context) initCtx.lookup("java:comp/env");
+            // Look up our data source
+            ds = (DataSource) envCtx.lookup("jdbc/DSTest-c3p0");
+            // Allocate and use a connection from the pool
+            connection = ds.getConnection();
+            String sql = "Select user,pass from users where user=? and pass=?";
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setString(1, user_input);
+            pst.setString(2, pass_input);
             ResultSet rs = pst.executeQuery();
             if (rs.next()) {
-                out.println("Correct login credentials");
-            } 
-            else {
-                out.println("Incorrect login credentials");
+                //RequestDispatcher rd = request.getRequestDispatcher("Welcome");   //class
+
+                PreparedStatement pst_role = connection.prepareStatement("select role from users where user=? and pass=?");
+                pst_role.setString(1, user_input);
+                pst_role.setString(2, pass_input);
+                ResultSet rs_role = pst_role.executeQuery();
+                if (rs_role.next()) {
+                    role = rs_role.getString("role");
+                    System.out.println("role is : " + role);
+
+                    if (role.equals("admin")) {
+
+                        RequestDispatcher rd = request.getRequestDispatcher("/admin.html");  //web page
+                        rd.forward(request, response);
+                    }
+                    if (role.equals("user")) {
+
+                        RequestDispatcher rd = request.getRequestDispatcher("/user.html");  //web page
+                        rd.forward(request, response);
+                    }
+                }
+            } else {
+                out.println("Username or Password incorrect");
+                RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+                rd.include(request, response);
             }
-        } 
-        catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+        } catch (NamingException ex) {
+            Logger.getLogger(MySQLConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            try {
+                Thread.sleep(5000);
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ src/main/java/com/mora/login_page_29juin/Welcome.java
@@ -0,0 +1,25 @@
+package com.mora.login_page_29juin;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Welcome extends HttpServlet {
+    
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Welcome user");
+      }  
+}
+
+
+
+
+
+
+
+
+
